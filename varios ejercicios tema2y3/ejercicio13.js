85375d608b9f7909fb6aed9890a6efd2e3576a00
xmomoex <moimorex@gmail.com>
2023-11-03 06:44:45 +0100

    s

--- varios ejercicios tema2y3/ejercicio13.js
+++ varios ejercicios tema2y3/ejercicio13.js
@@ -6,62 +6,67 @@ let codPostal = new Map([
     [11500, "Puerto de Santa María"]
 ])
 //le decimos que inserte la opcion
-let opcion = -1;
+let opcion = 1;
 
 
 
-while (opcion < 0 || opcion > 5) {
-opcion = prompt("Inserte la opción: ");
-}
+    while (opcion >0 && opcion <6) {
+        opcion = prompt("Inserte la opción: 1 buscar cod postal, 2 eliminar cod postal, 3 listar cod postal, 4 modificar cod postal, 5 salir ");
+    
 
-switch (opcion) {
-    case "1":
-        //le pedimos que inserte el codigo postal que busca
-        let codBuscado = prompt("Inserte el código postal que busca");
-        let encontrado = false;
-        for (let [codigo, nombre] of codPostal) {
-            if (codigo == codBuscado) {
-                document.write('El código ha sido encontrado');
-                encontrado = true;
+    switch (opcion) {
+        case "1":
+            //le pedimos que inserte el codigo postal que busca
+            let codBuscado = prompt("Inserte el código postal que busca");
+            let encontrado = false;
+            for (let [codigo, nombre] of codPostal) {
+                if (codigo == codBuscado) {
+                    document.write('El código ha sido encontrado');
+                    encontrado = true;
+                }
             }
-        }
-        if (encontrado == false) {
-            document.write("El código no ha sido encontrado");
-        }
-        break;
+            if (encontrado == false) {
+                document.write("El código no ha sido encontrado");
+            }
+            break;
 
-    case "2":
-        //le pedimos que inserte el codigo postal que busca
-        let codEliminar = prompt("Inserte el código postal que busca");
-        encontrado = false;
-        for (let [codigo, nombre] of codPostal) {
-            if (codigo == codEliminar) {
-                codPostal.delete(codEliminar);
-                document.write('El código ha sido eliminado');
-                encontrado = true;
+        case "2":
+            //le pedimos que inserte el codigo postal que busca
+            let codEliminar = prompt("Inserte el código postal que busca");
+            let encontradoE = false;
+            for (let [codigo, nombre] of codPostal) {
+                if (codigo == codEliminar) {
+                    codPostal.delete(codigo);
+                    document.write('El código ha sido eliminado');
+                    encontradoE = true;
+                }
             }
-        }
-        if (encontrado == false) {
-            document.write("El código no ha sido encontrado");
-        }
-        break;
-    case "3":
-        //le pedimos que inserte el codigo postal que busca
-        for (nombre of codPostal) {
-            document.write(nombre+"<br>");
-        }
-        break;
-    case "4":
-        let codModificar = prompt("Inserte el código postal que busca");
-        encontrado = false;
-        for (let [codigo, nombre] of codPostal) {
-            if (codigo == codModificar) {
-                nombre = prompt("inserte el nuevo nombre del código")
-                encontrado = true;
+            if (encontradoE == false) {
+                document.write("El código no ha sido encontrado");
             }
-        }
-        if (encontrado == false) {
-            document.write("El código no ha sido encontrado");
-        }
-        break;
-}
+            break;
+        case "3":
+            //le pedimos que inserte el codigo postal que busca
+            for (nombre of codPostal) {
+                document.write(nombre + "<br>");
+            }
+            break;
+        case "4":
+            let codModificar = prompt("Inserte el código postal que busca");
+            encontradoM = false;
+            for (let [codigo, nombre] of codPostal) {
+                if (codigo == codModificar) {
+                    codPostal.nombre = prompt("inserte el nuevo nombre del código")
+                    encontradoM = true;
+                }
+            }
+            if (encontradoM == false) {
+                document.write("El código no ha sido encontrado");
+            }
+            break;
+            
+        case "5":
+            document.write("fin del programa");
+            opcion = -1;
+            break;
+    }}
