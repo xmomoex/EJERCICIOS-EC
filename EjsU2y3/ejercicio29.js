0468d5b09b78d27ba4c7a1dd805747df83eb0e0a
xmomoex <moimorex@gmail.com>
2023-11-03 07:37:38 +0100

    s

--- EjsU2y3/ejercicio29.js
+++ EjsU2y3/ejercicio29.js
@@ -1,15 +1,16 @@
 //creamos la funcion
 function palabra_busqueda (cadena,busqueda){
-
+    //guardamos las palabras
     var palabras = cadena.split(' ');
     var cont = 0;
+    //recorrremos las palabras y si la palabra coincide el contador aumenta
     for (let i = 0; i < palabras.length; i++) {
         var palabra = palabras[i];
         if (palabra == busqueda){
             cont++;
         }
     }
-
+    //sacamos por pantalla
     document.write(busqueda + " se encontró "+ cont + " veces");
 }
 
