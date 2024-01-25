cf53a7248992d32aa1ffa6550d0cb22cc1f28d0b
xmomoex <moimorex@gmail.com>
2024-01-25 10:47:37 +0100

    s

---
+++ actividadest6/ejercicio6.16.js
@@ -0,0 +1,5 @@
+//declaro la variable
+let parrafo = document.getElementById("parrafo");
+//modifico los estilos como quiera
+parrafo.style.color = "purple";
+parrafo.style.fontSize = "30px";
---
+++ actividadest6/ejercicio6.17.js
@@ -0,0 +1,7 @@
+let lista = document.getElementById("lista");
+//guado el primero y el ultimo
+let primerNombre = lista.firstElementChild.textContent;
+let ultimoNombre = lista.lastElementChild.textContent;
+//los devuelvo por consola
+console.log("Primer nombre:", primerNombre);
+console.log("Último nombre:", ultimoNombre);
