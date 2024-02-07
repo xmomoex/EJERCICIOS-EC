1a3c2c74138c93f9e633638c543b58d1d1c2ceb5
xmomoex <moimorex@gmail.com>
2024-02-07 19:34:16 +0100

    s

---
+++ actividadest6/ejercicio6.21.js
@@ -0,0 +1,8 @@
+let dataWith = prompt("Introduce el ancho");
+let dataHeigt = prompt("Introduce el alto");
+let dataColor = prompt("Introduce el color de borde");
+
+const caja = document.querySelector("div");
+caja.dataset.with = dataWith;
+caja.dataset.heigt = dataHeigt;
+caja.dataset.bordercolor = dataBorderColor;
