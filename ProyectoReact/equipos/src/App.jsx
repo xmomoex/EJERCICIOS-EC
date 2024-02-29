2d5678d20315717be7079f92bdaf9d1882545172
xmomoex <moimorex@gmail.com>
2024-02-29 14:06:47 +0100

    s

--- ProyectoReact/equipos/src/App.jsx
+++ ProyectoReact/equipos/src/App.jsx
@@ -1,15 +1,17 @@
 import { Header } from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
+      <Header title="mi pagina web" show={true}>
+        <h1>Esto es un header</h1>
+        <Footer />
+      </Header>
       <section>
         <p>Este es el section</p>
       </section>
-      <Footer />
     </>
   );
 }
--- ProyectoReact/equipos/src/components/Header.jsx
+++ ProyectoReact/equipos/src/components/Header.jsx
@@ -1,9 +1,17 @@
 import "./Header.css";
+import PropTypes from "prop-types";
 
-export const Header = () => {
+export const Header = ({ children, title, show }) => {
   return (
     <header className="header">
-      <h1 className="title">header</h1>
+      <h3 className="title">{title || "titulo por defecto"}</h3>
+      {children}
+      <p>Este texto se va a mostar solo si el prop show es true</p>
     </header>
   );
+};
+
+Header.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
 };
