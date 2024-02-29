af8e305da9df55344b83b06fe0411bdba3bf03b4
xmomoex <moimorex@gmail.com>
2024-02-29 11:00:05 +0100

    s

---
+++ ProyectoReact/equipos/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>bienvenidos a equipos</h1>
+      </header>
+      <section>
+        <p>Este es el section</p>
+      </section>
+      <footer>
+        <p>este es el footer</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
