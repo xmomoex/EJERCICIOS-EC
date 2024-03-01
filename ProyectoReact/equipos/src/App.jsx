b94321b544b336f26e6e3b78a7649635d63bec9e
xmomoex <moimorex@gmail.com>
2024-03-01 13:19:19 +0100

    s

--- ProyectoReact/equipos/src/App.jsx
+++ ProyectoReact/equipos/src/App.jsx
@@ -1,17 +1,15 @@
 import { Header } from "./components/Header";
 import Footer from "./components/Footer/Footer";
+import Section from "./components/Section/Section";
 import "./App.css";
+
 
 function App() {
   return (
     <>
-      <Header title="mi pagina web" show={false}>
-        <h1>Esto es un header</h1>
-        <Footer />
-      </Header>
-      <section>
-        <p>Este es el section</p>
-      </section>
+      <Header title="mi pagina web" show={false}/>
+        <Section/>
+      <Footer />
     </>
   );
 }
