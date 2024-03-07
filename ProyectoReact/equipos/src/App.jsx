90937eca32d3d057b522e67fdc2fbedf37a8d215
xmomoex <moimorex@gmail.com>
2024-03-07 15:08:54 +0100

    s

--- ProyectoReact/equipos/src/App.jsx
+++ ProyectoReact/equipos/src/App.jsx
@@ -1,13 +1,12 @@
 import { Header } from "./components/Header";
 import Footer from "./components/Footer/Footer";
-import Main from "./components/Main/Main";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header title="mi pagina web" show={false} />
-      <Main />
+
       <Footer />
     </>
   );
